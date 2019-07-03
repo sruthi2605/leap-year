7711e61b44a7f9899b335b853055c1c71304975b
I46851 <sruthi.sathyanarayananan@bnpparibasfortis.com>
2019-07-03 15:08:22 +0200

    Logic added to check if 2016 is leap year

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
+import LeapYearFinder from "../LeapYearFinder/LeapYearFinder";
 
-const App = () => <div className="" />;
+const App = () => <div><LeapYearFinder/></div>;
 
 export default App;
--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
+import LeapYearFinder from '../LeapYearFinder/LeapYearFinder'
 
 describe('App', () => {
+
     it('should render a <div />', () => {
         const wrapper = shallow(<App />);
         expect(wrapper.find('div').length).toEqual(1);
+    });
+
+    it('should render the LeapYearFinder Component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.containsMatchingElement(<LeapYearFinder />)).toEqual(true);
     });
 });
