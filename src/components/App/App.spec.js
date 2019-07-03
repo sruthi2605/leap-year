c9a14416b8e48ff5147f1a8b37e59f3458edfc7a
I46851 <sruthi.sathyanarayananan@bnpparibasfortis.com>
2019-07-03 15:13:22 +0200

    Refactored Test suites by adding beforeEach

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -5,13 +5,14 @@ import LeapYearFinder from '../LeapYearFinder/LeapYearFinder'
 
 describe('App', () => {
 
+    let wrapper;
+    beforeEach(() => wrapper = shallow(<App />));
+
     it('should render a <div />', () => {
-        const wrapper = shallow(<App />);
         expect(wrapper.find('div').length).toEqual(1);
     });
 
     it('should render the LeapYearFinder Component', () => {
-        const wrapper = shallow(<App />);
         expect(wrapper.containsMatchingElement(<LeapYearFinder />)).toEqual(true);
     });
 });
